210e1aa490ddec4cfddc8f8e127a34c8992773a2
jiya <jiahofficial64@gmail.com>
2024-10-12 01:02:36 +0530

    commit

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -6,12 +6,13 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAu8flScBmDzG8q97kibU_7fQw-IXLB5p8",
-  authDomain: "crashchain.firebaseapp.com",
-  projectId: "crashchain",
-  storageBucket: "crashchain.appspot.com",
-  messagingSenderId: "402747656561",
-  appId: "1:402747656561:web:78bb3c5fa2ef9f20718ed0"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId:import.meta.env.VITE_APP_ID
+
 };
 
 // Initialize Firebase
